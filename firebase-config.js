18abdb494b60e935008aae17d7bf4a38dafe81be
Alex Law <alexlaw@Alexs-MacBook-Pro-4.local>
2018-01-13 15:48:43 -0800

    Added Firebase

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+{/*<script src="https://www.gstatic.com/firebasejs/4.8.2/firebase.js"></script>*/}
+    {/*<script>*/}
+{/*// Initialize Firebase*/}
+{/*var config = {*/}
+    {/*apiKey: "AIzaSyDprQgPC5OWoDzrHzIXABeWahxvfySm6ls",*/}
+    {/*authDomain: "podpitch.firebaseapp.com",*/}
+    {/*databaseURL: "https://podpitch.firebaseio.com",*/}
+    {/*projectId: "podpitch",*/}
+    {/*storageBucket: "",*/}
+    {/*messagingSenderId: "885746487787"*/}
+{/*};*/}
+{/*firebase.initializeApp(config);*/}
+{/*</script>*/}
